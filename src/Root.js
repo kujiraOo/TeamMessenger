3bb2b2db3646e301b05b8424c97189216ad27ab3
Martin Anderson <m.anderson.developer@gmail.com>
2017-03-06 16:32:35 +0200

    shadows added to main coolumns

--- src/Root.js
+++ src/Root.js
@@ -61,5 +61,6 @@ import {automateLogin} from './automatescript/automateLogin'
 
 //automateLogin("hruser");
 
-automateLogin("cashierLead");
+//automateLogin("cashierLead");
+            automateLogin("hr");
 
